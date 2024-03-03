39440e19c0917ca35f3977f41a1f5f7718acb0bb
Brandon Kwamou <brandonkwamou@gmail.com>
2024-03-03 05:06:14 +0100

    Backend clean-up! : remove all unecessary file to host the questions

--- src/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-  return (
-    <header className="app-header">
-      <img src="/quizzapp.png" alt="Quiz logo" />
-      <h1>The QuizzApp</h1>
-    </header>
-  );
-}
--- src/components/Main.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-interface MainProps {
-  children: ReactNode;
-}
-
-export default function Main({ children }: MainProps) {
-  return <div className="main">{children}</div>;
-}
